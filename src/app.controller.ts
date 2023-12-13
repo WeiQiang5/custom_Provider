45228c3aa5f95cb147fbf7b1e768303b5e7edf77
caiweiqiang <1429388864@qq.com>
2023-12-13 23:09:49 +0800

    feat:增加对module模块中provider的操作

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // 以下两种写法是基于provides下面的provide不是字符串的情况下
+  // 注入AppService，写法1
+  // constructor(private readonly appService: AppService) {}
+  // 写法2:
+  // @Inject(AppService)
+  // private readonly appService: AppService;
+
+  // 字符串情况下
+  // 写法1:
+  // constructor(@Inject('app_service') private readonly appService: AppService) {}
+  // 写法2:
+  @Inject('app_service')
+  private readonly appService: AppService;
+
+  @Inject('person')
+  private readonly person: { name: string; age: number };
+
+  @Inject('person2')
+  private readonly person2: { name: string; age: number };
 
   @Get()
   getHello(): string {
+    console.log(this.person, this.person2);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,50 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  // 这是写法1，是写法2的简写
+  // providers: [AppService],
+  // 写法2
+  // providers: [
+  //   {
+  //     provide: AppService,
+  //     useClass: AppService,
+  //   },
+  // ],
+  // 写法3，provide用作字符串
+  providers: [
+    AppService,
+    {
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    // 这里还可以直接指定一个值，让ioc容器来注入
+    {
+      provide: 'person',
+      useValue: {
+        name: 'aaa',
+        age: 20,
+      },
+    },
+    // provider的值可能是动态产生的，nest也同样支持
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'bbb',
+          desc: 'cccc',
+        };
+      },
+    },
+    {
+      provide: 'person3',
+      useFactory(person: { name: string }, appService: AppService) {
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['person', AppService],
+    },
+  ],
 })
 export class AppModule {}
